3ec96e3dc011ec904750c17630dfb5f87d0ebc23
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2013-11-29 12:50:09 +0100

    Remove extra requirements

--- setup.py
+++ setup.py
@@ -9,25 +9,12 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid>=1.4',
     'pyramid_jinja2',
-    'pyramid_debugtoolbar',
-    'pyramid_beaker==0.7',
-    'pyramid_tm==0.7',
-    'colander==1.0b1',
-    'Babel==1.3',
-    'lingua==1.5',
-    'pymongo==2.6.3',
-    'py-bcrypt==0.4',
-    'gunicorn==18.0',
-    'pyramid_sockjs==0.3.9',
-    'celery==3.0.24',
-    'celery-with-mongodb==3.0',
-    'pyramid_celery==1.3',
-    'cornice==0.15',
+    'waitress',
 ]
 
-setup(name='gecoscc',
+setup(name='gecoscc-mockups',
       version='0.0',
-      description='gecoscc',
+      description='gecoscc-mockups',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
@@ -52,9 +39,9 @@ setup(name='gecoscc',
       test_suite="gecoscc",
       entry_points="""\
       [paste.app_factory]
-      main = gecoscc:main
+      main = gecosccmockups:main
       [console_scripts]
-      pmanage = gecoscc.management:main
+      pmanage = gecosccmockups.management:main
       """,
       paster_plugins=['pyramid'],
       )
